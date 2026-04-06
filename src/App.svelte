cad3f8eae845a282e1a6617dc7c05b65c0265c39
Martin <mr.mmaurer@gmail.com>
2026-04-06 22:45:03 +0200

    Scaffolded Svelte app

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  const sections = [
+    { label: "Basic float grid", id: "basic float-grid" },
+  ] as const satisfies {
+    label: string;
+    id: string;
+  }[];
+</script>
+
+<header>
+  <h1>Grids</h1>
+
+  <!-- <ul>
+    <li></li>
+  </ul> -->
+</header>
+<main>
+  <section>
+    <h2>Basic float grind</h2>
+  </section>
+</main>
+
+<style>
+  @layer components {
+  }
+</style>
