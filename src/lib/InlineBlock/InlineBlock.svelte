bb3c696892b0117ecfc7c830217ab9351ddf52f9
Martin <mr.mmaurer@gmail.com>
2026-04-24 23:37:46 +0200

    Added inline block grid description

--- src/lib/InlineBlock/InlineBlock.svelte
+++ src/lib/InlineBlock/InlineBlock.svelte
@@ -10,6 +10,42 @@
   let { numberOfColumns }: InlineBlockProps = $props();
 </script>
 
+{#snippet descriptionContent()}
+  <p>
+    This <em>inline-block-based</em> grid uses <em>inline margins</em> to create gutters between
+    columns. There are multiple approaches to remove the whitespace between inline-block columns.
+    One approach is to set <code class="code">letter-spacing: -1em</code> on the parent row and
+    reset it to <em>normal</em> in each column. Another is to set
+    <code class="code">font-size: 0</code> on the parent row and reset it in each column. A third
+    option is to strip the whitespace from the markup using HTML comments. Finally, setting the
+    parent row to <code class="code">display: table</code> causes some browsers to ignore whitespace entirely.
+  </p>
+  <p>
+    Each column except the first has an inline-start margin equal to the gutter width. This creates
+    consistent spacing between columns while keeping the first column flush with the container edge.
+  </p>
+  <ul class="new-column">
+    <li>
+      <a href="https://github.com/esr360/Kayzen-GS"
+        >KayzenGS (<em>letter-spacing</em> and <em>display-table</em>)</a
+      >
+    </li>
+    <li>
+      <a href="https://pure-css.github.io/grids/">Yahoo Pure (<em>letter-spacing</em>)</a>
+    </li>
+    <!-- <li>
+    <a href="https://csswizardry.com/csswizardry-grids/"
+      >csswizardry-grids (uses comment trick, padding for gutters)</a
+    >
+  </li>
+  <li>
+    <a href="https://github.com/daneden/toast"
+      >Toast grid (uses comment trick, padding for gutters)</a
+    >
+  </li> -->
+  </ul>
+{/snippet}
+
 {#snippet gridContent()}
   <Row class="row">
     {#each { length: numberOfColumns }}
@@ -23,8 +59,8 @@
   style:--gutter-size-total={'calc((var(--number-of-columns) - 1) * var(--gutter-size))'}
   style:--size-of-column={'calc((100% - var(--gutter-size-total)) / var(--number-of-columns))'}
 >
-  {@render title('Inline block grids (KayzenGS, Yahoo Pure, CSSWizardry-grids)')}
-  {@render description()}
+  {@render title('Inline block grids')}
+  {@render description(descriptionContent)}
   {@render grid(gridContent)}
   {@render markup(css)}
 </section>
